1a231d734c1b3cf9011a603eb10a656c3626aa88
Anees <aneesawan34@yahoo.com>
2017-12-02 19:20:43 +0500

    learning branches

---
+++ 41.py
@@ -0,0 +1,12 @@
+def donots(b):
+    for limit in range(1,b+1):
+        limit = "English"
+        print(limit)
+donots(7)
+
+def all(x):
+    for tab in range(1,x+1):
+        tab="Science"
+        print(tab)
+all(10)
+
